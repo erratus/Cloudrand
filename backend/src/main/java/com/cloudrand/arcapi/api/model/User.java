7bbcce8ec6712fa03b54dcbf56d123072787e69f
harshkatiyar19 <harshkatiyar03.w@gmail.com>
2025-03-12 15:06:42 +0530

    Merge remote-tracking branch 'origin/jwt-verification' into jwt-verification
    # Conflicts:
    #	backend/pom.xml
    #	backend/src/main/java/com/cloudrand/arcapi/api/controller/AdminController.java
    #	backend/src/main/java/com/cloudrand/arcapi/api/controller/FileController.java
    #	backend/src/main/java/com/cloudrand/arcapi/api/controller/UserController.java
    #	backend/src/main/java/com/cloudrand/arcapi/api/model/File.java
    #	backend/src/main/java/com/cloudrand/arcapi/repository/UserRepository.java
    #	backend/src/main/java/com/cloudrand/arcapi/service/UserService.java

--- backend/src/main/java/com/cloudrand/arcapi/api/model/User.java
+++ backend/src/main/java/com/cloudrand/arcapi/api/model/User.java
@@ -1,7 +1,6 @@
 package com.cloudrand.arcapi.api.model;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
